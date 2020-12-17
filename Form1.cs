3ff2b62e258861187bdf659e8e157911c4ce4d46
Johan Erdös <jo8558er-s@lu.se>
2020-12-17 15:43:27 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using BlackjackWpf.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+
+namespace BlackjackWpf
+{
+    public partial class Form1 : Form
+    {
+        public PictureBox[] playerPictures;
+        public PictureBox[] dealerPictures;
+        public const string imagePath = @"Cards/";
+        public const string imgPathBackground = @"Pictures/";
+        Card card = new Card();
+        List<Card> playerHand = new List<Card>();
+        List<Card> dealerHand = new List<Card>();
+        public Form1()
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            playerPictures = new PictureBox[2];
+            dealerPictures = new PictureBox[1];
+
+            Image imgTable = Image.FromFile(imgPathBackground + "table.jpg");
+
+            imgTable = ResizeImage(imgTable, new Size(1000, 2000));
+
+            this.BackgroundImage = imgTable;
+
+            Image imgBlackChip = Image.FromFile(imgPathBackground + "chipBlack.jpg");
+            imgBlackChip = ResizeImage(imgBlackChip, new Size(100, 100));
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Card c1 = new Card("J", 10, "S");
+            Card c2 = new Card("Q", 10, "C");
+            Card c3 = card.PrintCard();
+            playerHand.Add(c1);
+            playerHand.Add(c2);
+            dealerHand.Add(c3);
+            CreatePlayerControls(playerHand);
+            CreateDealerControls(dealerHand);
+            DisplayPlayerControls();
+            DisplayDealerControls();
+        }
+
+        private void CreatePlayerControls(List<Card> playerHand)
+        {
+
+            for (int i = 0; i < playerHand.Count; i++)
+            {
+                string cardRank = playerHand[i].Rank;
+                string cardSuit = playerHand[i].Suit;
+
+                var newPictureBox = new PictureBox();
+                newPictureBox.Height = 110;
+                newPictureBox.Width = 100;
+                playerPictures[i] = SizeImage(newPictureBox, cardRank, cardSuit);
+            }
+
+        }
+
+        private void CreateDealerControls(List<Card> dealerHand)
+        {
+            for(int i = 0; i < dealerHand.Count; i++)
+            {
+                string cardRank = dealerHand[i].Rank;
+                string cardSuit = dealerHand[i].Suit;
+
+                var newPictureBox = new PictureBox();
+                newPictureBox.Height = 110;
+                newPictureBox.Width = 100;
+                dealerPictures[i] = SizeImage(newPictureBox, cardRank, cardSuit);
+            }
+        }
+
+        public PictureBox SizeImage(PictureBox pb, string cardRank, string cardSuit)
+        {
+
+            string concatPath = cardRank + cardSuit;
+
+            Image img = Image.FromFile(imagePath + concatPath + ".jpg");
+            img = ResizeImage(img, new Size(pb.Height, pb.Width));
+            pb.Image = img;
+            pb.SizeMode = PictureBoxSizeMode.CenterImage;
+            pb.Top = 100;
+
+
+            return pb;
+        }
+
+        public static Image ResizeImage(Image image, Size size)
+        {
+            return (Image)(new Bitmap(image, size));
+
+        }
+        private void DisplayPlayerControls()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                playerPictures[i].Left = (i * 18) + 100;
+                this.Controls.Add(playerPictures[i]);
+            }
+        }
+
+        private void DisplayDealerControls()
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                dealerPictures[i].Left = (i * 18) + 500;
+                this.Controls.Add(dealerPictures[i]);
+            }
+        }
+
+        private void Hit(object sender, EventArgs e)
+        {
+            int value = 0;
+            Card c4 = card.PrintCard();
+            playerHand.Add(c4);
+            CreatePlayerControls(playerHand);
+            DisplayPlayerControls();
+
+            foreach (Card c in playerHand)
+            {
+                value += c.Value;
+            }
+            label1.Text = value.ToString();
+
+        }
+
+        private void Stay(object sender, EventArgs e)
+        {
+            int value = 0;
+
+            Card c5 = card.PrintCard();
+            dealerHand.Add(c5);
+            CreateDealerControls(dealerHand);
+            DisplayDealerControls();
+
+            foreach (Card c in dealerHand)
+            {
+                value += c.Value;
+            }
+
+            label2.Text = value.ToString();
+
+        }
+
+        private void Bet50(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Bet100(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Bet20(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Model/Card.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackjackWpf.Model
+{
+    class Card
+    {
+        private string _rank;
+        private int _value;
+        
+        private string _suit;
+
+        
+       
+
+        public Card(string _rank, int _value, string _suit)
+        {
+            Rank = _rank;
+            Value = _value;
+            Suit = _suit;
+        }
+
+        public Card()
+        {
+
+        }
+        public string Rank 
+        {
+            get { return _rank; }
+            set { _rank = value; }
+        }
+
+        public int Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        public string Suit
+        {
+            get { return _suit; }
+            set { _suit = value; }
+        }
+
+        public Card PrintCard()
+        {
+            string[] rank = new string[] { "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2" };
+            string[] suits = new string[] { "H", "C", "D", "S" };
+            int value = 0;
+
+            Random rand = new Random();
+            int indexRank = rand.Next(0, rank.Length);
+            int indexSuit = rand.Next(0, suits.Length);
+
+            string r = rank[indexRank];
+            string s = suits[indexSuit];
+
+            if(r.Equals("A") || r.Equals("K") || r.Equals("Q") || r.Equals("J"))
+            {
+                value = ConvertRank(r);
+            }
+            else
+            {
+                value = Convert.ToInt32(r.ToString());
+            }
+
+            Card card = new Card(r, value, s);
+
+            return card;
+            
+        }
+
+        public int ConvertRank(string rank)
+        {
+            int value = 0;
+            if (rank.Equals("A"))
+            {
+                value = 11;
+                
+            }
+            else
+            {
+                value = 10;
+                
+            }
+            return value;
+            
+        }
+
+
+    }
+}
